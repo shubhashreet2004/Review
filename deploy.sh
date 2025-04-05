5a1dda9c137d4969f94a932aef6e9b36595c27cc
shubhashreet2004 <135111992+shubhashreet2004@users.noreply.github.com>
2025-04-05 09:16:09 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u shubhashree1311 -p 
+    docker login -u shubhashree1311 -p shubha@1311
     docker tag test shubhashree1311/review
     docker push shubhashree1311/review
     docker-compose up -d
